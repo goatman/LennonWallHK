395d45f4ce48e2fbec70b0165f17dc9871f996b8
Free Hong Kong <lennonwallhkar@gmail.com>
2019-08-28 12:40:02 +0800

    .gitignore fix

--- Pods/Target Support Files/Pods-LennonWallHK/Pods-LennonWallHK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LennonWallHK : NSObject
-@end
-@implementation PodsDummy_Pods_LennonWallHK
-@end
--- Pods/Target Support Files/Pods-LennonWallHKUITests/Pods-LennonWallHKUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LennonWallHKUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LennonWallHKUITests
-@end
